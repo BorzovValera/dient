840918f4b2e32ec92b6736ebcab178b971d5411e
BorzovValera <70910769+BorzovValera@users.noreply.github.com>
2023-05-14 20:49:45 +0300

    Add files via upload

---
+++ 1.php
@@ -0,0 +1,217 @@
+<!DOCTYPE html>
+<html lang="RU">
+
+<head>
+  <meta charset="UTF-8">
+  <title>Галерея дверей</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.0.2/css/bootstrap.min.css">
+</head>
+
+<body>
+  <div class="container my-5">
+    <h1 class="text-center my-5">ГАЛЕРЕЯ ДВЕРЕЙ</h1>
+
+    <div class="row row-cols-1 row-cols-md-4 g-4">
+      <div class="col">
+        <div class="card h-100">
+          <img src="image1.jpg" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Image Title 1</h5>
+            <p class="card-text">Image Description 1</p>
+          </div>
+        </div>
+      </div>
+
+      <div class="col">
+        <div class="card h-100">
+          <img src="image2.jpg" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Image Title 2</h5>
+            <p class="card-text">Image Description 2</p>
+          </div>
+        </div>
+      </div>
+      
+      <!-- Continue with 18 more cards here... -->
+      <div class="col">
+        <div class="card h-100">
+          <img src="image3.jpg" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Image Title 2</h5>
+            <p class="card-text">Image Description 2</p>
+          </div>
+        </div>
+      </div>
+      <div class="col">
+        <div class="card h-100">
+          <img src="image4.jpg" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Image Title 2</h5>
+            <p class="card-text">Image Description 2</p>
+          </div>
+        </div>
+      </div>
+      <div class="col">
+        <div class="card h-100">
+          <img src="image5.jpg" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Image Title 2</h5>
+            <p class="card-text">Image Description 2</p>
+          </div>
+        </div>
+      </div>
+     <div class="col">
+        <div class="card h-100">
+          <img src="image1.jpg" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Image Title 1</h5>
+            <p class="card-text">Image Description 1</p>
+          </div>
+        </div>
+      </div>
+
+      <div class="col">
+        <div class="card h-100">
+          <img src="image2.jpg" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Image Title 2</h5>
+            <p class="card-text">Image Description 2</p>
+          </div>
+        </div>
+      </div>
+      
+      <!-- Continue with 18 more cards here... -->
+      <div class="col">
+        <div class="card h-100">
+          <img src="image3.jpg" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Image Title 2</h5>
+            <p class="card-text">Image Description 2</p>
+          </div>
+        </div>
+      </div>
+      <div class="col">
+        <div class="card h-100">
+          <img src="image4.jpg" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Image Title 2</h5>
+            <p class="card-text">Image Description 2</p>
+          </div>
+        </div>
+      </div>
+      <div class="col">
+        <div class="card h-100">
+          <img src="image5.jpg" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Image Title 2</h5>
+            <p class="card-text">Image Description 2</p>
+          </div>
+        </div>
+      </div>
+<div class="col">
+        <div class="card h-100">
+          <img src="image1.jpg" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Image Title 1</h5>
+            <p class="card-text">Image Description 1</p>
+          </div>
+        </div>
+      </div>
+
+      <div class="col">
+        <div class="card h-100">
+          <img src="image2.jpg" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Image Title 2</h5>
+            <p class="card-text">Image Description 2</p>
+          </div>
+        </div>
+      </div>
+      
+      <!-- Continue with 18 more cards here... -->
+      <div class="col">
+        <div class="card h-100">
+          <img src="image3.jpg" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Image Title 2</h5>
+            <p class="card-text">Image Description 2</p>
+          </div>
+        </div>
+      </div>
+      <div class="col">
+        <div class="card h-100">
+          <img src="image4.jpg" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Image Title 2</h5>
+            <p class="card-text">Image Description 2</p>
+          </div>
+        </div>
+      </div>
+      <div class="col">
+        <div class="card h-100">
+          <img src="image5.jpg" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Image Title 2</h5>
+            <p class="card-text">Image Description 2</p>
+          </div>
+        </div>
+      </div>
+<div class="col">
+        <div class="card h-100">
+          <img src="image1.jpg" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Image Title 1</h5>
+            <p class="card-text">Image Description 1</p>
+          </div>
+        </div>
+      </div>
+
+      <div class="col">
+        <div class="card h-100">
+          <img src="image2.jpg" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Image Title 2</h5>
+            <p class="card-text">Image Description 2</p>
+          </div>
+        </div>
+      </div>
+      
+      <!-- Continue with 18 more cards here... -->
+      <div class="col">
+        <div class="card h-100">
+          <img src="image3.jpg" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Image Title 2</h5>
+            <p class="card-text">Image Description 2</p>
+          </div>
+        </div>
+      </div>
+      <div class="col">
+        <div class="card h-100">
+          <img src="image4.jpg" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Image Title 2</h5>
+            <p class="card-text">Image Description 2</p>
+          </div>
+        </div>
+      </div>
+      <div class="col">
+        <div class="card h-100">
+          <img src="image5.jpg" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Image Title 2</h5>
+            <p class="card-text">Image Description 2</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+
+  <!-- Bootstrap JS -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.0.2/js/bootstrap.min.js"></script>
+</body>
+
+</html>
